bb217cc2f8738abbe105660ba95572a9cc3cc466
WindyBindy <134945382+WindyBindy@users.noreply.github.com>
2025-03-10 17:55:28 +0100

    makeing textarea

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 const settingsOpenBtn = document.getElementById('settings-btn')
-
+const textarea = document.getElementById('input')
+const download = document.getElementById('download-btn')
+let textar = textarea.value
+let text = JSON.stringify();
 settingsOpenBtn.onclick = function () {
 	window.location.href = './pages/settings.html'
 }
+
+download.onclick = function () {
+
+}
+
+
